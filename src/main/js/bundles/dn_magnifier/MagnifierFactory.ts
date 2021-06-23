76a2d1957afd83a2d29ec7336b09caeb02e362a7
Matthias Stein <m.stein@conterra.de>
2021-06-23 14:29:24 +0200

    Reformat MagnifierFactory.ts

--- src/main/js/bundles/dn_magnifier/MagnifierFactory.ts
+++ src/main/js/bundles/dn_magnifier/MagnifierFactory.ts
@@ -16,46 +16,40 @@
 
 import View from "esri/views/View";
 import { InjectedReference } from "apprt-core/InjectedReference";
-
+import ScreenPoint from "esri/geometry/ScreenPoint";
 
 // Interface definition for magnifier properties outlined in manifest.json/app.json
 interface PropertiesObject {
     factor: number,
     maskEnabled: boolean,
     maskUrl: string,
-    offset: __esri.ScreenPoint,
+    offset: ScreenPoint,
     overlayEnabled: boolean,
     overlayUrl: string,
-    position: __esri.ScreenPoint,
+    position: ScreenPoint,
     size: number,
     visible: boolean
-};
-
+}
 
 export default class MagnifierFactory {
 
     private _properties: PropertiesObject;
     private _mapWidgetModel: InjectedReference;
 
-
     /**
      * Function used to show magnifier
      * uses magnifier properties specified in manifest.json or app.json
      */
     showMagnifier(): void {
-
         // access view
         this._getView().then((view: View) => {
-
             // set properties of the view's magnifier according to manifest.json/app.json
             view.magnifier.factor = this._properties.factor;
             view.magnifier.maskEnabled = this._properties.maskEnabled;
             view.magnifier.maskUrl = this._properties.maskUrl;
-            debugger
             view.magnifier.offset = this._properties.offset;
             view.magnifier.overlayEnabled = this._properties.overlayEnabled;
             view.magnifier.overlayUrl = this._properties.overlayUrl;
-            view.magnifier.position = this._properties.position;
             view.magnifier.size = this._properties.size;
 
             // finally, make magnifier visible
@@ -68,6 +62,7 @@ export default class MagnifierFactory {
                     y: event.y
                 };
             });
+            view.cursor = "crosshair";
         });
     }
 
@@ -75,22 +70,21 @@ export default class MagnifierFactory {
      * Function used to hide magnifier
      */
     hideMagnifier(): void {
-
         // access view
         this._getView().then((view: View) => {
-
             // turn off visibility of the view's magnifier
             view.magnifier.visible = false;
         });
+        view.cursor = "default";
     }
 
     /**
-     * Helper function used to access views
+     * Helper function used to access the esri/views/View instance
      */
     _getView(): Promise<View> {
-        const mapWidgetModel: InjectedReference = this._mapWidgetModel;
+        const mapWidgetModel = this._mapWidgetModel;
 
-        return new Promise((resolve, reject) => {
+        return new Promise((resolve) => {
             if (mapWidgetModel.view) {
                 resolve(mapWidgetModel.view);
             } else {
